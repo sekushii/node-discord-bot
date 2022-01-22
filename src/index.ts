bab831e0f79874b0c703926fdd0c20965faade15
sekushii <daniel.756@hotmail.com>
2022-01-22 09:16:51 -0300

    attempt to fix lint-staged not working

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import 'module-alias/register';
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Ready!');
+  console.log('Ready1!');
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
