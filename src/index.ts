2b15e19fed724c0ead99e70533261793ddfe8842
sekushii <daniel.756@hotmail.com>
2022-01-22 13:58:11 -0300

    replace module-alias as pathing dependency, change dotenv to dev dep

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import 'dotenv/config';
 import * as Discord from 'discord.js';
-import 'module-alias/register';
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Ready1!');
+  console.log('Ready!');
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
